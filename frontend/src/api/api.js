2cc03fbe316316958d5e766be59097409cbaa294
ad1tya0478 <aditya.50478@gmail.com>
2026-03-27 22:40:28 +0530

    Updated the URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE = "http://localhost:8000/api";
+const BASE = "https://dataviz-pidn.onrender.com";
 
 // Upload a file (CSV or XLSX)
 export const uploadFile = (file) => {
